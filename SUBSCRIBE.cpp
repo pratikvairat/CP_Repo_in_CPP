07cbcd7d592a4370621db2956f55466637b5fd82
Pratik Vairat <84719258+pratikvairat@users.noreply.github.com>
2022-09-25 22:43:32 +0530

    Create SUBSCRIBE.cpp

---
+++ SUBSCRIBE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int t,x;
+	cin>>t;
+	for(int i=0;i<t;i++){
+		cin>>x;
+		if(x<=30){
+			cout<<"NO\n";
+		}else{
+			cout<<"YES\n";
+		}
+	}
+}
